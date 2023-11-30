f81115950ad213e96555272e1eab84e8c0260353
ChaplyginM <ChaplyginM@mail.ru>
2023-11-30 15:24:30 +0300

    Скрипт зацикливания видео

---
+++ ffmpeg-Scripts/LoopVideoToItself.ps1
@@ -0,0 +1,22 @@
+param([String[]] $filenames,
+     $fadedur,
+     $ffmpeg_path)
+
+foreach ($filename in $filenames)
+{
+    $f_inf = New-Object System.IO.FileInfo($filename)
+    $duration = &($ffmpeg_path+"ffprobe.exe") -v error -show_entries format=duration -of csv=p=0 $filename
+    $bitrate = &($ffmpeg_path+"ffprobe.exe") -v error -show_entries stream=bit_rate -of csv=p=0 $filename
+
+ #   Write-Output $bitrate
+
+    if ($duration -gt 0)
+    { 
+        &($ffmpeg_path+"ffmpeg.exe") -loglevel info -i "$filename" -filter_complex `
+            "[0]split[body][pre];
+             [body]trim=$fadedur,setpts=PTS-STARTPTS[main];
+             [pre]trim=duration=$fadedur,format=yuva420p,fade=d=$fadedur\:alpha=1,setpts=PTS+($($duration - $fadedur*2)/TB)[jt];
+             [main][jt]overlay" -an -b:v $bitrate `
+             $($f_inf.DirectoryName+"\"+$f_inf.BaseName+"_l$fadedur"+$f_inf.Extension)
+    }
+}
---
+++ ffmpeg-Scripts/README.md
@@ -0,0 +1,5 @@
+Script makes video looped to itself, using crossfading (seamless looping last frame to begining)
+
+Usage:
+LoopVideoToItself.ps1 -filename <filename> -filenames <file-list> -ffmpeg_path <path to ffmpeg's bin folder>
+
