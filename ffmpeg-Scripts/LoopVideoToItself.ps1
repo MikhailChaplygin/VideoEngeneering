7cfe9b359c456fd8890019303e79537b9a6c9b4a
ChaplyginM <ChaplyginM@mail.ru>
2023-12-05 17:39:11 +0300

    update LoopVideoToItself.ps1 script

--- ffmpeg-Scripts/LoopVideoToItself.ps1
+++ ffmpeg-Scripts/LoopVideoToItself.ps1
@@ -8,17 +8,19 @@ foreach ($filename in $filenames)
     $duration = &($ffmpeg_path+"ffprobe.exe") -v error -show_entries format=duration -of csv=p=0 $filename
     $bitrate = &($ffmpeg_path+"ffprobe.exe") -v error -select_streams v:0 -show_entries stream=bit_rate -of csv=p=0 $filename
 
-    Write-Output $bitrate
-
-    if ($duration -gt 0)
+    if ($($duration-(2*$fadedur)) -gt 0)
     { 
-        $ExecutionString = $($ffmpeg_path+"ffmpeg.exe")+' -loglevel info -i '+ $filename+ ' -filter_complex '+ `
-            """[0]split[body][pre];
-             [body]trim=$fadedur,setpts=PTS-STARTPTS[main];
-             [pre]trim=duration=$fadedur,format=yuva420p,fade=d=$fadedur\:alpha=1,setpts=PTS+($($duration - $fadedur*2)/TB)[jt];
-             [main][jt]overlay"" -an -b:v "+$bitrate+ ' '+`
-             $($f_inf.DirectoryName+"\"+$f_inf.BaseName+"_l$fadedur"+$f_inf.Extension)
-        Write-Output($ExecutionString)
-        &($ExecutionString)
+        $ExecutionString = $($ffmpeg_path+"ffmpeg.exe")
+        $ExecParam = " -loglevel info -i `"$filename`" -filter_complex " +
+                        "`"[0]split[body][pre];" +
+                        "[body]trim=$fadedur,setpts=PTS-STARTPTS[main];" +
+                        "[pre]trim=duration=$fadedur,format=yuva420p,fade=d=$fadedur\:alpha=1,setpts=PTS+($($duration - $fadedur*2)/TB)[jt];" +
+                        "[main][jt]overlay`" -an -b:v "+ $bitrate + " " +
+                        "`"$($f_inf.DirectoryName+"\"+$f_inf.BaseName+$("_l"+$fadedur+$f_inf.Extension))`""
+        Invoke-Expression "& `"$ExecutionString`" $ExecParam"
+    }
+    else 
+    {
+        Write-Output("Failed! Clip duration($duration) doesn't allow to choose this fade duration($fadedur)")
     }
 }
