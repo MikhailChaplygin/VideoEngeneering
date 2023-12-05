64136a0b00b6866f0d81d86db6dd60ad532b46c3
ChaplyginM <ChaplyginM@mail.ru>
2023-12-05 20:59:17 +0300

    Исправлено: 1. Переконвертация в тот же кодек. 2. Исправлен Pixelformat, что приводило к дерганию при переходе

--- ffmpeg-Scripts/LoopVideoToItself.ps1
+++ ffmpeg-Scripts/LoopVideoToItself.ps1
@@ -7,20 +7,21 @@ foreach ($filename in $filenames)
     $f_inf = New-Object System.IO.FileInfo($filename)
     $duration = &($ffmpeg_path+"ffprobe.exe") -v error -show_entries format=duration -of csv=p=0 $filename
     $bitrate = &($ffmpeg_path+"ffprobe.exe") -v error -select_streams v:0 -show_entries stream=bit_rate -of csv=p=0 $filename
-
+    $codec = &($ffmpeg_path+"ffprobe.exe") -v error -select_streams v:0 -show_entries stream=codec_name -of csv=p=0 $filename
+    
     if ($($duration-(2*$fadedur)) -gt 0)
     { 
         $ExecutionString = $($ffmpeg_path+"ffmpeg.exe")
         $ExecParam = " -loglevel info -i `"$filename`" -filter_complex " +
                         "`"[0]split[body][pre];" +
                         "[body]trim=$fadedur,setpts=PTS-STARTPTS[main];" +
-                        "[pre]trim=duration=$fadedur,format=yuva420p,fade=d=$fadedur\:alpha=1,setpts=PTS+($($duration - $fadedur*2)/TB)[jt];" +
-                        "[main][jt]overlay`" -an -b:v "+ $bitrate + " " +
+                        "[pre]trim=duration=$fadedur,format=yuva420p,fade=d=$fadedur`:alpha=true,setpts=PTS+($($duration - $fadedur*2)/TB)[jt];" +
+                        "[main][jt]overlay, format=yuva420p`" -an -c:v $codec -b:v $bitrate " +
                         "`"$($f_inf.DirectoryName+"\"+$f_inf.BaseName+$("_l"+$fadedur+$f_inf.Extension))`""
         Invoke-Expression "& `"$ExecutionString`" $ExecParam"
     }
     else 
     {
-        Write-Output("Failed! Clip duration($duration) doesn't allow to choose this fade duration($fadedur). duration>2*fadedur")
+        Write-Output("Failed! Clip duration($duration) doesn't allow to choose this fade duration($fadedur). Clip duration must be greater then (2*fadedur)")
     }
 }
